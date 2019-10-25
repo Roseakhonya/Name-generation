22e4d7f7750b3f55f0cf0f5ea8fc9bd44068a24f
Roseakhonya <akhonyarose@gmail.com>
2019-10-25 14:49:41 +0300

    Add a validation function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-var dd = document.getElementById('dd').value;
-var mm = document.getElementById('mm').value;
-var cc=
+function (find Akan-name) {
+    var dd = document.getElementById('dd').value;
+    var mm = document.getElementById('mm').value;
+    var cc = document.getElementById('cc').value;
+    var yy = document.getElementById('yy').value;
+    var femaleGender = document.getElementById('femaleGender').value;
+    var maleGender = document.getElementById('maleGender').value;
+}
+
+var femaleNames[""]
+var maleNames[""]
