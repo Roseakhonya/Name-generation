7763e1d9ca5559a2ea3a4d0e20b219a6bbe2c13a
Roseakhonya <akhonyarose@gmail.com>
2019-10-28 10:44:36 +0300

    Add lists and function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,49 @@
-function (find Akan-name) {
-    var dd = document.getElementById('dd').value;
-    var mm = document.getElementById('mm').value;
-    var cc = document.getElementById('cc').value;
-    var yy = document.getElementById('yy').value;
-    var femaleGender = document.getElementById('femaleGender').value;
-    var maleGender = document.getElementById('maleGender').value;
-}
+function findName() {
+  var dd = parseInt(document.getElementById("dd").value);
+  var mm = parseInt(document.getElementById("mm").value);
+  var yy = parseInt(document.getElementById("yy").value);
+  var femaleGender = document.getElementById("femaleGender");
+  var maleGender = document.getElementById("maleGender");
+  var date = new Date(yy + "-" + mm + "-" + dd);
+  var dayBorn = date.getDay();
 
-var femaleNames[""]
-var maleNames[""]
+  var maleNames = [
+    "Kwasi",
+    "Kwado",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
+  ];
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  if (dd < 1 || dd > 31) {
+    alert("Enter valid day");
+  } else if ((dd.toString()).length !== 4) {
+    alert("Enter a valid year");
+  } else if (maleGender.checked) {
+    alert(
+      "Hello there, your day of birth is " +
+        daysOfTheWeek[dayBorn] +
+        " and your Akan name is " +
+        maleNames[dayBorn]
+    );
+  } else {
+    alert(
+      "Hello there, your day of birth is " +
+        daysOfTheWeek[dayBorn] +
+        " and your Akan name is " +
+        maleNames[dayBorn]
+    );
+  }
+}
